06d6dc361fba7419395a34183f3eedcbe97ed148
EndersonPro <enjaviga@gmail.com>
2020-03-31 22:20:45 -0500

    Ahora si xd todo configurado eche

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! v1';
+    return 'Hello World! v2 xdxd';
   }
 }
