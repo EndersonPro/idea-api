8367c8b2f554d5aedfea94db2f4180e16eaeff2b
EndersonPro <enjaviga@gmail.com>
2020-04-05 21:51:11 -0500

    Añadiendo Auth Guard :ok_hand:

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, Post, Body, UsePipes } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UsePipes,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDTO } from './user.dto';
 import { ValidationPipe } from 'src/shared/validation.pipe';
+import { AuthGuard } from 'src/shared/auth.guard';
+import { User } from './user.decorator';
 
 @Controller()
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get('api/users')
-  showAllUsers() {
+  @UseGuards(new AuthGuard())
+  showAllUsers(@User() user) {
+    console.log(user);
     return this.userService.showAllUsers();
   }
 
