aac09a5c333674fdf8729ee7960d2692850ef29a
EndersonPro <enjaviga@gmail.com>
2020-04-05 21:00:20 -0500

    Añadiendo User Module :ok_hand:

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDTO } from './user.dto';
+import { ValidationPipe } from 'src/shared/validation.pipe';
 
-@Controller('user')
-export class UserController {}
+@Controller()
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('api/users')
+  showAllUsers() {
+    return this.userService.showAllUsers();
+  }
+
+  @Post('login')
+  @UsePipes(new ValidationPipe())
+  login(@Body() data: UserDTO) {
+    return this.userService.login(data);
+  }
+
+  @Post('register')
+  @UsePipes(new ValidationPipe())
+  register(@Body() data: UserDTO) {
+    return this.userService.register(data);
+  }
+}
