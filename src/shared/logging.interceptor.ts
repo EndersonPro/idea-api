127ba516531b40f71577bea211c2d6a3612ab652
EndersonPro <enjaviga@gmail.com>
2020-04-07 23:20:21 -0500

    Support loggin for grapql

--- src/shared/logging.interceptor.ts
+++ src/shared/logging.interceptor.ts
@@ -7,17 +7,34 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const req = context.switchToHttp().getRequest();
-    const method = req.method;
-    const url = req.url;
     const now = Date.now();
+    if (req) {
+      const method = req.method;
+      const url = req.url;
+      return next
+        .handle()
+        .pipe(tap(() => Logger.log(`${method} ${url} ${Date.now() - now}ns`)));
+    } else {
+      const ctx: any = GqlExecutionContext.create(context);
+      const resolverName = ctx.constructorRef.name;
+      const info = ctx.getInfo();
 
-    return next
-      .handle()
-      .pipe(tap(() => Logger.log(`${method} ${url} ${Date.now() - now}ns`)));
+      return next
+        .handle()
+        .pipe(
+          tap(() =>
+            Logger.log(
+              `${info.parentType} "${info.fieldName}" ${Date.now() - now}ns`,
+              resolverName,
+            ),
+          ),
+        );
+    }
   }
 }
