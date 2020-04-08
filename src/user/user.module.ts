cab2c66adc4a6c48440cef6cd8b230f1685d10e5
EndersonPro <enjaviga@gmail.com>
2020-04-07 23:20:49 -0500

    Import CommentModule and CommentEntity

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,16 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
+import { UserResolver } from './user.resolver';
+import { CommentEntity } from 'src/comment/comment.entity';
+import { CommentModule } from 'src/comment/comment.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
-  providers: [UserService],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, CommentEntity]),
+    CommentModule,
+  ],
   controllers: [UserController],
+  providers: [UserService, UserResolver],
 })
 export class UserModule {}
